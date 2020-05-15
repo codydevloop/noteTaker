98f77627f6f3bb3c11dd27347af104b1617068b0
CodyHoover <codydevloop@gmail.com>
2020-05-15 06:31:55 -0700

    created HTML routes

--- server.js
+++ server.js
@@ -2,19 +2,28 @@
 // =============================================================
 const path = require("path");
 const express = require("express");
-const app = express();
-// Heroku PORT config 
+const app = express();  // Heroku PORT config 
+
 const PORT = process.env.PORT || 8080;
 
 // Routes
 // =============================================================
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/index.html"));
-    // res.send("WTF");
 });
 
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+// app.post("/api/notes", (req, res) =>{
+//     const newNote = req.body;
+//     console.log(newNote);
+// });
 
 
+// Listener 
+// =============================================================
 app.listen(PORT, ()=>{
     console.log("App listening on PORT " + PORT);
 });
