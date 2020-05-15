b9a4370dce0b743dfeb38aa91ef315119e648d29
CodyHoover <codydevloop@gmail.com>
2020-05-15 07:10:02 -0700

    created API routes

--- server.js
+++ server.js
@@ -2,9 +2,15 @@
 // =============================================================
 const path = require("path");
 const express = require("express");
-const app = express();  // Heroku PORT config 
+const fs = require("fs");
+const app = express();  
 
-const PORT = process.env.PORT || 8080;
+app.use(express.json()); // let express know that incomming/post is JSON
+app.use(express.urlencoded({extended:true})); //required when taking data from HTML forms
+
+const PORT = process.env.PORT || 8080;  // Heroku PORT config 
+
+const notesArr = [];
 
 // Routes
 // =============================================================
@@ -16,10 +22,25 @@ app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
-// app.post("/api/notes", (req, res) =>{
-//     const newNote = req.body;
-//     console.log(newNote);
-// });
+app.get("/api/notes", (req, res) => {
+    fs.readFile(path.join(__dirname, "./db/db.json"),"utf8",(err, data) => {
+        if (err) throw err;
+        console.log(data);
+      });
+    
+    res.send("its working");
+
+});
+
+
+app.post("/api/notes", (req, res) =>{
+    const newNote = req.body;
+    console.log(newNote);
+    //notesArr.push(newNote);
+    //console.log(JSON.stringify(notesArr));
+    res.json(newNote);
+    
+});
 
 
 // Listener 
