b59e62ca16090b87e7c98ec4ecc8e9a592356955
CodyHoover <codydevloop@gmail.com>
2020-05-18 05:37:51 -0700

    added npm uuid, each post request will be assigned a uniquie id

--- routes/api/postnotes.js
+++ routes/api/postnotes.js
@@ -2,20 +2,27 @@ const express = require("express");
 const path = require("path");
 const router = express.Router();
 const fs = require("fs");
+const { v4: uuidv4 } = require('uuid');
 // const app = require("../../server");
 
 
 router.post("/api/notes", (req, res) => {
     let newNote = req.body;
-    let tempNoteArr = [];
+    let tempNoteArr = [];  // clear array
     // console.log(req.body);
 
+    //==========================
+    // (1) read db.json and place into a temp variable tempNoteArr
+    //=========================
     fs.readFile(path.join(__dirname, "../../db/db.json"),"utf8", function(err, data) {
         if (err){
             throw err;
         } 
-
         tempNoteArr = JSON.parse(data);
+        //==========================
+        // (2) add incomming note to the temp arrat
+        //=========================
+        newNote.id = uuidv4();
         tempNoteArr.push(newNote); 
         console.log(tempNoteArr);
         // console.log(tempNoteArr);
@@ -23,6 +30,10 @@ router.post("/api/notes", (req, res) => {
         // console.log("Cody"+ tempNoteArr.title);
         // console.log(typeof(tempNoteArr));
         // console.log("type:" + typeof(tempNoteArr));
+        
+        //==========================
+        // overwrite db.json with updated tempNoteArray information
+        //=========================
         fs.writeFile(path.join(__dirname, "../../db/db.json"),JSON.stringify(tempNoteArr),(err) => {
             if (err){
                 throw err;
